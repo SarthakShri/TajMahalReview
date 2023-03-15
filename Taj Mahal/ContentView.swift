a73b43f31b36956e502d0e4a4aafb046bf419bcf
Sarthak Shrivastava <sarthakrishu@gmail.com>
2023-03-15 15:43:45 +0530

    Initial Commit

---
+++ Taj Mahal/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Taj Mahal
+//
+//  Created by Sarthak Shrivastava on 15/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Taj Mahal/Taj_MahalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Taj_MahalApp.swift
+//  Taj Mahal
+//
+//  Created by Sarthak Shrivastava on 15/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Taj_MahalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
